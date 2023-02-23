7cef3c6e4a2ac52264e6ea9a0aa4032ab39ee96b
Nikita <ashtonashtonkachur@gmail.com>
2023-02-23 14:32:43 -0500

    Second commit

--- polls/models.py
+++ polls/models.py
@@ -1,5 +1,7 @@
-from django.db import models
+import datetime
 
+from django.db import models
+from django.utils import timezone
 # Create your models here.
 
 
@@ -7,9 +9,18 @@ class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
+    def __str__(self):
+        return f'Question: {self.question_text}'
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f'Choice: {self.choice_text}'
 
--- polls/views.py
+++ polls/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import get_object_or_404
+from .models import Question
+from django.http import Http404
 
 # Create your views here.
 
 
 # main page
 def index(request):
-    return HttpResponse('Today i will go trough this tutorial and understand how create such an apps')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
 
-# Detail page
+# Detail page that displays the question text
 def detail(request, question_id):
-    return HttpResponse('You are looking at the question of question %s' % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 # Results page
